d0f560d64a8009e078a987ce0c04c7d5ced57b34
Matheus Bosa <matheusbozza@gmail.com>
2020-03-02 16:36:16 -0300

    Cria arquivo contendo app e teste

---
+++ submarino.py
@@ -0,0 +1,16 @@
+import unittest
+
+
+class Submarino(object):
+    pass
+
+
+class SubmarinoTest(unittest.TestCase):
+
+    def testFoo(self):
+        sub = Submarino()
+        self.assertEqual(1, 1)
+
+
+if __name__ == "__main__":
+    unittest.main()
